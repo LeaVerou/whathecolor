9496f6980537b17a3184fcd0d6dab6dcc3d0312b
Lea Verou <lea@verou.me>
2026-06-07 04:15:10 -0400

    Move proximity above

--- components/color-board.js
+++ components/color-board.js
@@ -1,13 +1,14 @@
 const template = /* html */`
 <div class="color-board">
-	<div class="colors">
-		<div class="color solution" title="Color to guess" :style="{ background: target }"></div>
-		<div class="color your-color" title="Your guess" :style="{ background: guess }"></div>
-	</div>
 	<label class="proximity" :class="{ success: solved }" :style="{ '--proximity': proximity }" :title="deltaTitle">
 		<span class="caption">Proximity <strong class="percent">{{ percent }}</strong></span>
 		<progress :value="proximity" max="1"></progress>
 	</label>
+	<div class="colors">
+		<div class="color solution" title="Color to guess" :style="{ background: target }"></div>
+		<div class="color your-color" title="Your guess" :style="{ background: guess }"></div>
+	</div>
+
 </div>
 `;
 
